33fd88d63da23e50626888977171a33569082ce7
HR Utsha <128457387+hr-utsha@users.noreply.github.com>
2025-04-15 20:59:35 +0600

    Delete cart directory

--- cart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('', views.view_cart, name='view_cart'),
-    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('update/<int:item_id>/', views.update_quantity, name='update_quantity'),
-]
